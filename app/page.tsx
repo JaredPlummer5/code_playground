652c83188072b4114ecd560d1a523845044ae37e
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-10-02 16:57:05 -0500

    Trying to fix stuff

--- app/page.tsx
+++ app/page.tsx
@@ -31,14 +31,15 @@ const DemoPage = () => {
           <h1 className="text-9xl font-bold mb-24 text-white">
             Welcome to{' '}
             <span className="border-t-4 border-r-4 border-b-4 border-l-4 border-gradient-to-r from-cyan-500 to-blue-500 border-blue-300">
-              BitBox
-            </span>{' '}
+            BitBox
+          </span>{' '}
           </h1>
           <p className="mb-6 text-white">
             BitBox is your coding platform and grading platform of the future.
           </p>
           <Link href="/demo">
-            <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105 hover:animate-glow">
+          <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105 hover:animate-glow">
+
               Try Now!
             </button>
           </Link>
